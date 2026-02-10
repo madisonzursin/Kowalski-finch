08341637cbe447581b70a48a939f340a29dd7985
Ashaila_J <ashaila.jackson@marquette.edu>
2026-02-10 15:02:15 -0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+from BirdBrain import Finch
+
+finch = Finch('A')
+finch.setMove('F', 20, 100)
+finch.setTurn('R', 360, 30)
+finch.playNote(60, 0.5)
+finch.setMove('F', 20, 100)
+finch.setTurn('R', 360, 30)
+finch.playNote(70, 0.5)
+
